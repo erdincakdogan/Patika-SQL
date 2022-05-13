969ec4e7b38e66a42cc427033f3e649ddcfbad12
erdincakdogan <82614878+erdincakdogan@users.noreply.github.com>
2022-05-13 23:47:38 +0300

    Create Odev3.sql

---
+++ Odev3.sql
@@ -0,0 +1,8 @@
+SELECT * FROM country
+WHERE country LIKE 'A&a';
+SELECT * FROM country
+WHERE country LIKE '______n';
+SELECT*FROM film
+WHERE title ILIKE '%T%T%T%T' 
+SELECT*FROM film
+WHERE title LIKE 'C%' AND length >90 and rental_Rate =2.99
